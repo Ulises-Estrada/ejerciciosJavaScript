c30133d0b961f2d9c9a681879cd2e895f379d10f
Ulises Estrada <ulisesestradaceja@gmail.com>
2024-07-07 15:26:50 -0600

    Cambios Eliminacion de módulos de aritmética y de constantes

--- js/modulos.js
+++ js/modulos.js
@@ -1,18 +1,4 @@
-/* import saludar, { Saludar, PI, usuario } from "./constantes.js";
-import { aritmetica as calculos } from "./aritmetica.js";
+import { contarCaracteres, arrayTextos } from "./funcionesEjercicios.js";
 
-console.log("Archivo modulos.js");
-
-console.log(PI, usuario);
-//console.log(aritmetica.sumar(3, 4));
-console.log(calculos.sumar(3, 4));
-console.log(calculos.restar(3, 4));
-saludar();
-let saludo = new Saludar();
-saludo;
- */
-
-import { contarCaracteres } from "./funcionesEjercicios.js";
-
-const cadena = "Hola Mundo";
-contarCaracteres(cadena);
+contarCaracteres("Hola Mundo");
+arrayTextos("hola que tal", " ");
